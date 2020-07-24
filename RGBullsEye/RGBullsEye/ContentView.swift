b6acc29747fc2a5ac3e58e370cfac0e496310af4
EricGolovin <ericgolovin@outlook.com>
2020-07-24 23:53:08 +0300

    Day 2.

--- RGBullsEye/RGBullsEye/ContentView.swift
+++ RGBullsEye/RGBullsEye/ContentView.swift
@@ -8,29 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var rTarget = Double.random(in: 0..<1)
-    @State var gTarget = Double.random(in: 0..<1)
-    @State var bTarget = Double.random(in: 0..<1)
-    @State var rGuess: Double
-    @State var gGuess: Double
-    @State var bGuess: Double
+    @State private var rTarget = Double.random(in: 0..<1)
+    @State private var gTarget = Double.random(in: 0..<1)
+    @State private var bTarget = Double.random(in: 0..<1)
+    @State private var rGuess = 0.5
+    @State private var gGuess = 0.5
+    @State private var bGuess = 0.5
     
-    @State var showAlert = false
+    @State private var showAlert = false
+    
+    @ObservedObject var timer = TimeCounter()
     
     var body: some View {
         VStack {
             HStack {
                 VStack {
                     Color(red: rTarget, green: gTarget, blue: bTarget).cornerRadius(10.0)
-                    Text("Match this color")
+                    self.showAlert ? Text("R: \(Int(rTarget * 255.0)) G: \(Int(gTarget * 255.0)) B: \(Int(bTarget * 255.0))") : Text("Match this color")
                 }
                 VStack {
-                    Color(red: rGuess, green: gGuess, blue: bGuess).cornerRadius(10.0)
-                    Text("R: \(Int(rGuess * 255.0)) G: \(Int(gGuess * 255.0)) B: \(Int(bGuess * 255.0))" )
+                    ZStack(alignment: .center) {
+                        Color(red: rGuess, green: gGuess, blue: bGuess).cornerRadius(10.0)
+                        Text("\(timer.counter)")
+                            .padding(.all, 5)
+                            .background(Color.white)
+                            .mask(Circle())
+                            .foregroundColor(.black)
+                    }
+                    Text("R: \(Int(rGuess * 255.0)) G: \(Int(gGuess * 255.0)) B: \(Int(bGuess * 255.0))")
                 }
             }.padding(.bottom)
             
-            Button(action: { self.showAlert = true }) {
+            Button(action: {
+                self.showAlert = true
+                timer.killTimer()
+            }) {
                 Text("Hit Me!").fontWeight(.bold).font(.title)
             }.alert(isPresented: $showAlert, content: {
                 Alert(title: Text("Your Score"), message: Text(String(computeScore())), primaryButton: .default(Text("New color"), action: {
@@ -40,12 +52,16 @@ struct ContentView: View {
                     rGuess = 0.5
                     gGuess = 0.5
                     bGuess = 0.5
+                    timer.startNewTimer()
                 }), secondaryButton: .cancel(Text("Cancel")))
             }).padding()
             
-            ColorSlider(value: $rGuess, textColor: .red)
-            ColorSlider(value: $gGuess, textColor: .green)
-            ColorSlider(value: $bGuess, textColor: .blue)
+            VStack {
+                ColorSlider(value: $rGuess, textColor: .red)
+                ColorSlider(value: $gGuess, textColor: .green)
+                ColorSlider(value: $bGuess, textColor: .blue)
+            }.padding(.horizontal)
+            
         }.padding()
     }
     
@@ -60,7 +76,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+        ContentView()
     }
 }
 
@@ -71,7 +87,9 @@ struct ColorSlider: View {
         HStack {
             Text("0").foregroundColor(textColor)
             Slider(value: $value)
+                .background(textColor)
+                .cornerRadius(10)
             Text("255").foregroundColor(textColor)
-        }.padding(.horizontal)
+        }
     }
 }
