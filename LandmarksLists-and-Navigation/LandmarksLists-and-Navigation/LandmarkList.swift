755b0306e6da2c48917f0b3e3221c6c4f02024a3
Eric Golovin <ericgolovin@outlook.com>
2020-08-23 23:43:33 +0300

    Add Building Lists and Navigation files

---
+++ LandmarksLists-and-Navigation/LandmarksLists-and-Navigation/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  LandmarksLists-and-Navigation
+//
+//  Created by Eric Golovin on 23.08.2020.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone 11 Pro"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
---
+++ LandmarksLists-and-Navigation/LandmarksLists-and-Navigation/Supporting Views/CircleView.swift
@@ -0,0 +1,26 @@
+//
+//  CircleView.swift
+//  LandmarksLists-and-Navigation
+//
+//  Created by Eric Golovin on 23.08.2020.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
